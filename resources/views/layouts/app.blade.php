44600c6402bc685b0508dac543b6e2be2ea98486
Kyle Anthony Nierras <20-1-01205@vsu.edu.ph>
2024-06-24 16:58:37 +0800

    Map bug error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,13 +11,13 @@
     <!-- Icon -->
     <link rel="icon" href="{{ asset('islaKonek.png') }}" type="image/png" />
 
-    {{-- Leaflet
+    
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
         integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
 
     <!-- Make sure you put this AFTER Leaflet's CSS -->
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script> --}}
+        integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script> 
 
 
     <!-- Fonts -->
@@ -36,7 +36,6 @@
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-    @stack('styles')
 </head>
 
 <body class="font-sans antialiased">
@@ -78,7 +77,6 @@
             </ul>
         </div>
     </footer>
-    @stack('scripts')
 </body>
 
 </html>
