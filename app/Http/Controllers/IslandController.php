27e91b883d0aa458b6fed170e3ea3e4e335c1db0
Kyle Anthony Nierras <20-1-01205@vsu.edu.ph>
2024-06-24 17:17:30 +0800

    Add Island

--- app/Http/Controllers/IslandController.php
+++ app/Http/Controllers/IslandController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Island;
 
 class IslandController extends Controller
 {
@@ -11,7 +12,8 @@ class IslandController extends Controller
      */
     public function index()
     {
-        return view('pages.island.index'); 
+        $islands = Island::all();
+        return view('pages.island.index', compact('islands')); 
     }
 
     /**
--- resources/views/pages/island/index.blade.php
+++ resources/views/pages/island/index.blade.php
@@ -5,12 +5,31 @@
         </h2>
     </x-slot>
 
+    @include('pages.island.partials.add-island-modal')
+
     <div class="py-5 md:py-12 content">
         <div class="max-w-7xl mx-auto h-full sm:px-6 lg:px-8">
-            <div class=" shadow-sm sm:rounded-lg h-full">
-                <div class="flex flex-col gap-4">
-                    
+            <div class="shadow-sm sm:rounded-lg h-full">
+                <div class="px-4 py-5 sm:px-6 flex justify-between items-center">
+                    <!-- Table Title and Subtitle -->
+                    <div>
+                        <h3 class="text-lg leading-6 font-medium text-gray-900">
+                            Island Table
+                        </h3>
+                        <p class="mt-1 max-w-2xl text-sm text-gray-500">
+                            This table shows the list of islands.
+                        </p>
+                    </div>
+                    <!-- Add Button -->
+                    <x-bladewind::button onclick="showModal('add-island')">
+                        Add Island
+                    </x-bladewind::button>
+                </div>
+                <div class="flex flex-col gap-4 m-3">
+                    <x-bladewind::table exclude_columns="id" searchable="true" search_placeholder="Search..."
+                        :data="$islands" class="rounded"/>
                 </div>
             </div>
         </div>
+    </div>
 </x-app-layout>
